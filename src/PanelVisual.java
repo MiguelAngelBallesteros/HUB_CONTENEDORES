3316817e5b03a10babc82c5c1729f91a13ef2ec8
MiguelAngelBallesteros <miguelangel.ballesteros@alu.uclm.es>
2023-04-21 20:24:58 +0200

    FIN

--- src/PanelVisual.java
+++ src/PanelVisual.java
@@ -2,7 +2,6 @@ import G03.Contenedores;
 import G03.Hub;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -20,43 +19,17 @@ public class PanelVisual extends JFrame implements ActionListener {
     private JTextField textField_EmpresaEmisora;
     private JTextField textField_Aduana;
     private JTextField textField_Prioridad;
+    private JTextField textField_Columna;
+    private JTextField textField_NumPais;
+    private JPanel PanelMain;
+    private JTextField textField_Texto;
 
     public PanelVisual() {
+        setContentPane(PanelMain);
         setTitle("Hub de Contenedores");
         setSize(800,500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridLayout(6,4));
-
-        JLabel textLabel_Id = new JLabel("ID:");
-        JLabel textLabel_Peso = new JLabel("Peso:");
-        JLabel textLabel_Pais = new JLabel("Pais:");
-        JLabel textLabel_Descricion = new JLabel("Descripcion:");
-        JLabel textLabel_Aduana = new JLabel("Control de Aduanas (True o False):");
-        JLabel textLabel_Prioridad = new JLabel("Prioridad (Valor entre 1 y 3):");
-        JLabel textLabel_EmpresaReceptora = new JLabel("Empresa Receptora:");
-        JLabel textLabel_EmpresaEmisora = new JLabel("Empresa Emisora:");
-
-        add(textLabel_Id);
-        add(textField_Id);
-        add(textLabel_Peso);
-        add(textField_Peso);
-        add(textLabel_Pais);
-        add(textField_Pais);
-        add(textLabel_Aduana);
-        add(textField_Aduana);
-        add(textLabel_Prioridad);
-        add(textField_Prioridad);
-        add(textLabel_Descricion);
-        add(textFile_Descripcion);
-        add(textLabel_EmpresaEmisora);
-        add(textField_EmpresaEmisora);
-        add(textLabel_EmpresaReceptora);
-        add(textField_EmpresaReceptora);
-        add(apilarContenedorButton);
-        add(desapilarContenedorButton);
-        add(mostrarPlanoDelHubButton);
-        add(mostrarDatosDeUnButton);
-        add(mostrarNúmeroDeContenedoresButton);
+        setVisible(true);
 
         mostrarDatosDeUnButton.addActionListener(this);
         mostrarPlanoDelHubButton.addActionListener(this);
@@ -65,15 +38,25 @@ public class PanelVisual extends JFrame implements ActionListener {
         desapilarContenedorButton.addActionListener(this);
     }
 
+    Hub HC=new Hub();
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource()==apilarContenedorButton){
-            Contenedores Cont=new Contenedores(Integer.parseInt(textField_Id.getText()),Integer.parseInt(textField_Peso.getText()),textField_Pais.getText(),Boolean.parseBoolean(textField_Aduana.getText()),Integer.parseInt(textField_Prioridad.getText()),textFile_Descripcion.getText().toCharArray(),textField_EmpresaEmisora.getText().toCharArray(),textField_EmpresaReceptora.getText().toCharArray());
-            Hub HC=new Hub();
+        if (e.getSource()==apilarContenedorButton) {
+            Contenedores Cont = new Contenedores(Integer.parseInt(textField_Id.getText()), Integer.parseInt(textField_Peso.getText()), textField_Pais.getText(), Boolean.parseBoolean(textField_Aduana.getText()), Integer.parseInt(textField_Prioridad.getText()), textFile_Descripcion.getText().toCharArray(), textField_EmpresaEmisora.getText().toCharArray(), textField_EmpresaReceptora.getText().toCharArray());
             HC.ApilarContenedor(Cont);
+            textField_Texto.setText("Se ha apilado el contenedor correctamente");
         }
-        else if (e.getSource()==desapilarContenedorButton){
-            int columna;
+        if (e.getSource()==desapilarContenedorButton){
+            HC.desapila_contenedor(Integer.parseInt(textField_Columna.getText())-1);
+        }
+        if (e.getSource()==mostrarPlanoDelHubButton){
+            HC.PlanoHub();
+        }
+        if (e.getSource()==mostrarDatosDeUnButton){
+            HC.mostrar_datos(Integer.parseInt(textField_Id.getText()));
+        }
+        if (e.getSource()==mostrarNúmeroDeContenedoresButton){
+            textField_NumPais.setText(HC.contenedores_pais(textField_NumPais.getText()));
         }
     }
 }
